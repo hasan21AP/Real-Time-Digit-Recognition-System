19bd4268382085d68cc79651cf66d6bef5f49e2a
hasan21AP <hasan.hla21@gmail.com>
2025-10-06 18:37:13 +0200

    Version:0.0.5 Delete fine_tuning.py and modify cam_nums.py

--- cam_nums.py
+++ cam_nums.py
@@ -7,7 +7,7 @@ import torchvision.transforms as transforms
 # Loading Model
 
 model = SimpleCNN()
-model.load_state_dict(torch.load("mnist_cnn.pth", weights_only=True))
+model.load_state_dict(torch.load("kaggle_printed_numbers.pth", map_location=torch.device('cpu'), weights_only=True))
 model.eval()
 
 
@@ -28,24 +28,39 @@ while True:
     if not ret:
         break
     
-    # Cut the frame to a square
-    h, w, _ = frame.shape
-    cx, cy = w // 2, h // 2
-    size = 150
-    roi = frame[cy-size:cy+size, cx-size:cx+size]
-    gray = cv2.cvtColor(roi, cv2.COLOR_BGR2GRAY)
-    _, thresh = cv2.threshold(gray, 128, 255, cv2.THRESH_BINARY_INV)
-    # Image Processing
-    img_pil = Image.fromarray(thresh)
-    input_tensor = transform(img_pil).unsqueeze(0)  # Add batch dimension
+    # Detecting the number
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    gray = cv2.GaussionBlur(gray, (5, 5), 0)
+    _, thresh = cv2.threshold(gray, 0, 255, cv2.THRESH_BINARY_INV + cv2.THRESH_OTSU)
+    contours, _ = cv2.findContours(thresh, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
     
+    for cnt in contours:
+        area = cv2.contourArea(cnt)
+        if area < 200:
+            continue
+
+        x, y, w, h = cv2.boundingRect(cnt)
+        aspect_ratio = w / float(h)
+        if aspect_ratio < 0.3 or aspect_ratio > 3.5:
+            continue
+
+        roi = gray[y:y+h, x:x+w]
+        img_pil = Image.fromarray(roi)
+        input_tensor = transform(img_pil).unsqueeze(0)
+
     with torch.no_grad():
         output = model(input_tensor)
-        predicted = torch.argmax(output, dim=1).item()
-    
-    # Display the predicted number
-    cv2.putText(frame, str(predicted), (cx, cy), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
-    cv2.rectangle(frame, (cx-size, cy-size), (cx+size, cy+size), (255, 0, 0), 2)
+        probs = torch.softmax(output, dim=1)
+        confidence, predicted_class = torch.max(probs, dim=1)
+        confidence = confidence.item()
+        predicted = predicted_class.item()
+
+    if confidence > 0.95:
+        cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2)
+        cv2.putText(frame, f"{predicted} ({confidence*100:.1f}%)",
+                    (x, y-10), cv2.FONT_HERSHEY_SIMPLEX, 0.8, (0,255,0), 2)
+
+        
     cv2.imshow('Camera', frame)
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
