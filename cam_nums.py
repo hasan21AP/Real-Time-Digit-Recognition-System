a566af242ad9ce7e8e3c3f177d8b15a1d9dfb52d
hasan21AP <hasan.hla21@gmail.com>
2025-10-14 14:56:04 +0200

    Version:0.1.2 Modify cam_nums.py --> adding yolo model to detect the numbers

--- cam_nums.py
+++ cam_nums.py
@@ -1,59 +1,77 @@
 import cv2
-from model.training import SimpleCNN
 import torch
 from PIL import Image
 import torchvision.transforms as transforms
-import easyocr
+from model.model import RecognizeNumbersModel
+from ultralytics import YOLO
+import time, os
 
-reader = easyocr.Reader(['en'])
 
-# Loading Model
 
-model = SimpleCNN()
-model.load_state_dict(torch.load("kaggle_printed_numbers.pth", map_location=torch.device('cpu'), weights_only=True))
+# Loading Models
+yolo = YOLO("yolo/runs/detect/yolo_digits/weights/best.pt")
+
+model = RecognizeNumbersModel()
+model.load_state_dict(torch.load("weights/kaggle_printed_digits.pth", map_location=torch.device('cpu'), weights_only=True))
 model.eval()
+
+LAP_CAM = 0
+ANDROID_CAM = "http://192.168.0.33:4747/video?fps=30"
+ANDROID_CAM_MADAR = "http://10.35.92.27:4747/video?fps=30"
 
 
 # Transformations
 transform = transforms.Compose([
     transforms.Grayscale(),        # قناة وحدة
-    transforms.Resize((28, 28)),
+    transforms.Resize((128, 128)),
     transforms.ToTensor(),
     # transforms.Lambda(lambda x: 1 - x),  # نقلب الألوان: أسود ↔ أبيض
     transforms.Normalize((0.5,), (0.5,))
 ])  
 
 # Open Camera
-cap = cv2.VideoCapture(0)
+cap = cv2.VideoCapture(ANDROID_CAM)
+os.makedirs("captures", exist_ok=True)
+
+last_capture_time = 0
+min_interval = 2.0
 
 while True:
     ret, frame = cap.read()
     if not ret:
         break
     
-    results = reader.readtext(frame)
-    for (bbox, text, prob) in results:
-        (top_left, top_right, bottom_right, bottom_left) = bbox
-        x1, y1 = map(int, top_left)
-        x2, y2 = map(int, bottom_right)
-        roi = frame[y1:y2, x1:x2]
-        if roi.size == 0:
-            continue
-        img_pil = Image.fromarray(cv2.cvtColor(roi, cv2.COLOR_BGR2RGB))
-        input_tensor = transform(img_pil).unsqueeze(0)
+    results = yolo(frame, verbose=False )
+    detections = results[0].boxes
+    
+    if len(detections) > 0:
+        for box in detections:
+            conf = float(box.conf[0])
+            if conf > 0.6:  # Confidence threshold
+                x1, y1, x2, y2 = map(int, box.xyxy[0])
+                if time.time() - last_capture_time > min_interval:
+                    roi = frame[y1:y2, x1:x2]
+                    filename = f"captures/{int(time.time())}.png"
+                    cv2.imwrite(filename, roi)
+                    print(f"📸 تم التقاط صورة عند {conf:.2f} وحفظها في {filename}")
+                    last_capture_time = time.time()
 
-        with torch.no_grad():
-            output = model(input_tensor)
-            probs = torch.softmax(output, dim=1)
-            confidence, predicted_class = torch.max(probs, dim=1)
-            confidence = confidence.item()
-            predicted = predicted_class.item()
-
-        if confidence > 0.9:
-                cv2.rectangle(frame, (x1, y1), (x2, y2), (0, 255, 0), 2)
-                cv2.putText(frame, f"{predicted} ({confidence*100:.1f}%)",
-                            (x1, y1 - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.8, (0,255,0), 2)
-
+                    # Analyze the captured ROI
+                    img = Image.fromarray(cv2.cvtColor(roi, cv2.COLOR_BGR2RGB))
+                    img = transform(img).unsqueeze(0)
+        
+                with torch.no_grad():
+                    output = model(img)
+                    probs = torch.softmax(output, dim=1)
+                    confidence, predicted = torch.max(probs, dim=1)
+                    
+                labels = ["0","1","2","3","4","5","6","7","8","9","none"]
+                number = labels[predicted.item()]
+                print(f"🔢 Predicted number: {number} (Confidence: {confidence.item():.2f})")
+            # Draw bounding box
+            cv2.rectangle(frame, (x1, y1), (x2, y2), (255, 0, 0), 2)
+            cv2.putText(frame, f"{conf:.2f}", (x1, y1 - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.6, (0,255,0), 2)
+            
         
     cv2.imshow('Camera', frame)
     if cv2.waitKey(1) & 0xFF == ord('q'):
--- main.py
+++ main.py
@@ -4,4 +4,4 @@ from model.test import load_model
 # print(model.model.parameters)
 
 # model.model_training(epochs=200)
-load_model()
+# load_model()
